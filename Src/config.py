9d34a6b92411d09259dd350b7b7d98c04d4cf4c9
Rezi <ytpabit@gmail.com>
2025-06-18 15:50:05 +0545

    Create config.py

---
+++ Src/config.py
@@ -0,0 +1,4 @@
+# Configuration settings
+DEFAULT_INTERFACE = "eth0"  # Default interface for Linux
+WINDOWS_INTERFACE = "Ethernet"  # Default interface for Windows
+CHANGE_INTERVAL = 300  # Default interval in seconds for auto-change
